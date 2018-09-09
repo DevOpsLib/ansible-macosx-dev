5d69e03398688a0156eb4a7f661282e71655f8d7
Pedro Cesar Azevedo <pedro.azevedo@xero.com>
2018-09-10 08:41:34 +1200

    Adds Ansible base playbook

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### Install Command Line Tools
+echo "?? :: CLI TOOLS"
+which make > /dev/null \
+    && echo "ok: CLI already installed!" \
+    || xcode-select --install
+
+### Install Ruby if not installed.
+echo "?? :: Homebrew"
+which brew > /dev/null \
+    && echo "ok: Brew already installed!" \
+    || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+### Install Ansible if not installed.
+echo "?? :: Ansible"
+which ansible > /dev/null \
+    && echo "ok: Ansible already installed!" \
+    || brew install ansible
+
+### Run the playbook
+ansible-playbook -i "localhost," -c local --become-method=su playbook.yml
